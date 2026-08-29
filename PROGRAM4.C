1b8733e2e7bb0d87e55f7b57e415642d9b8502f2
ady-verse <aadyag007@gmail.com>
2026-08-29 11:40:50 +0530

    Add files via upload

---
+++ PROGRAM4.C
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+  char ch;
+  printf("enter the character");
+  scanf("%c", &ch);
+  printf("The ascii value of %c = %d\n", ch, ch);
+  printf("AADYA GUPTA S2-50");
+  return 0;
+}
